8899e026dab5d18e51018def1f83bc289ce9cdf1
wuhengzhen <aaron_sunny@163.com>
2022-12-10 10:49:58 +0800

    代码提交

---
+++ boot-common-demo/src/main/java/com/github/humzroy/common/BaseApplication.java
@@ -0,0 +1,19 @@
+package com.github.humzroy.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author humzroy
+ * @date 2022/12/10 10:07
+ **/
+
+@SpringBootApplication
+public class BaseApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BaseApplication.class, args);
+    }
+}
---
+++ boot-common-demo/src/main/java/com/github/humzroy/common/controller/BaseController.java
@@ -0,0 +1,48 @@
+package com.github.humzroy.common.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.github.humzroy.framework.core.api.ApiResult;
+import com.github.humzroy.framework.core.api.TypeReferenceApiResult;
+import com.github.humzroy.framework.core.util.JacksonUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author humzroy
+ * @date 2022/12/10 10:07
+ **/
+@RestController
+@Slf4j
+public class BaseController {
+
+
+    @GetMapping("testJacksonType")
+    public ApiResult<?> testJacksonType() {
+
+        return ApiResult.ok();
+
+
+    }
+
+
+    private static String getList() {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            list.add("i" + i);
+        }
+        return JacksonUtil.toJsonString(ApiResult.ok(list));
+    }
+
+    public static void main(String[] args) {
+        String json = getList();
+        ApiResult<List> listApiResult = JacksonUtil.parseObject(json, new TypeReferenceApiResult<>(List.class));
+        ApiResult<List<String>> listApiResult1 = JacksonUtil.parseObject(json, new TypeReference<ApiResult<List<String>>>() {
+        });
+        System.out.println(listApiResult);
+    }
+}
